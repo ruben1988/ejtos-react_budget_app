7ec8b411343c56cf5f59997a2ce1f5c17364af33
Ruben <rubenpm88@gmail.com>
2024-07-03 11:18:05 +0200

    final

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -30,28 +30,28 @@ export const AppReducer = (state, action) => {
                     ...state
                 }
             }
-            case 'RED_EXPENSE':
-                const red_expenses = state.expenses.map((currentExp)=> {
-                    if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
-                        currentExp.cost =  currentExp.cost - action.payload.cost;
-                        budget = state.budget + action.payload.cost
-                    }
-                    return currentExp
-                })
-                action.type = "DONE";
-                return {
-                    ...state,
-                    expenses: [...red_expenses],
-                };
-            case 'DELETE_EXPENSE':
-            action.type = "DONE";
-            state.expenses.map((currentExp)=> {
-                if (currentExp.name === action.payload) {
-                    budget = state.budget + currentExp.cost
-                    currentExp.cost =  0;
+        case 'RED_EXPENSE':
+            const red_expenses = state.expenses.map((currentExp)=> {
+                if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
+                    currentExp.cost =  currentExp.cost - action.payload.cost;
+                    budget = state.budget + action.payload.cost
                 }
                 return currentExp
             })
+            action.type = "DONE";
+            return {
+                ...state,
+                expenses: [...red_expenses],
+            };
+        case 'DELETE_EXPENSE':
+        action.type = "DONE";
+        state.expenses.map((currentExp)=> {
+            if (currentExp.name === action.payload) {
+                budget = state.budget + currentExp.cost
+                currentExp.cost =  0;
+            }
+            return currentExp
+        })
             action.type = "DONE";
             return {
                 ...state,
